a1b328080788fbe9b1b5959a7d188c342058d098
mfeng9 <mfeng.pi@gmail.com>
2022-05-20 12:01:02 -0400

    revision of gripper env

--- examples/planar_gripper.py
+++ examples/planar_gripper.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from os import link
 from posixpath import join
 
 # from pybullet_tools.kuka_primitives import BodyPose, BodyConf, Command, get_grasp_gen, \
@@ -14,106 +15,166 @@ import pybullet as p
 import numpy as np
 import time
 import gym
+import pdb
 
+class CartesianGripper(object):
+  """
+  Implementation of CartesianGripper robot in PyBullet
+  """
+  def __init__(self, use_debug_camera=False, use_gui=True):
+    SDF_path = 'wsg_50_description/sdf/schunk_wsg_50_welded_fingers.sdf'
+    SDF_part_bottom = 'wsg_50_description/sdf/part_bottom.sdf'
+    SDF_part_top = 'wsg_50_description/sdf/part_top.sdf'
+    sim_id = connect(use_gui=True)
+    disable_real_time()
 
-SDF_path = '/home/night/apps/baselines/pybullet-planning/examples/schunk_wsg_50_welded_fingers.sdf'
-SDF_part_bottom = '/home/night/apps/baselines/pybullet-planning/examples/wsg_50_description/sdf/part_bottom.sdf'
-SDF_part_top = '/home/night/apps/baselines/pybullet-planning/examples/wsg_50_description/sdf/part_top.sdf'
-# DRAKE_IIWA_URDF = 'models/drake/iiwa_description/urdf/iiwa14_primitive_collision.urdf'
+    # DEBUG CAMERA SETTING
+    if use_debug_camera:
+      p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
+      p.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW,1)
+      p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 1)
+      p.configureDebugVisualizer(p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 1)
+      p.configureDebugVisualizer(p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, 1)
+      p.resetDebugVisualizerCamera(1.0, 0, -45, [0,0,0])  
+    draw_global_system()
+    
+    self.robots = []
 
+    with HideOutput():
+        # robot = load_model(DRAKE_IIWA_URDF) # KUKA_IIWA_URDF | DRAKE_IIWA_URDF
+        robot = p.loadSDF(SDF_path, 
+                    # globalScaling=1., 
+                    physicsClientId=0)
 
-sim_id = connect(use_gui=True)
-disable_real_time()
-
-
-# DEBUG CAMERA SETTING
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
-# p.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW,1)
-# p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 1)
-# p.configureDebugVisualizer(p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 1)
-# p.configureDebugVisualizer(p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, 1)
-# p.resetDebugVisualizerCamera(1.0, 0, -45, [0,0,0])  
-draw_global_system()
-
-with HideOutput():
-    # robot = load_model(DRAKE_IIWA_URDF) # KUKA_IIWA_URDF | DRAKE_IIWA_URDF
-    robot = p.loadSDF(SDF_path, 
-                # globalScaling=1., 
-                physicsClientId=0)
-
-    p.resetBasePositionAndOrientation(robot[0], 
-                                      [0,0,0.5], 
-                                    [ -0.7068252, 0, 0, 0.7073883 ],
-                                      )
-    floor = load_model('models/short_floor.urdf', fixed_base=True)
+        p.resetBasePositionAndOrientation(robot[0], 
+                                          [0,0,0.5], 
+                                        [ -0.7068252, 0, 0, 0.7073883 ],
+                                          )
+        floor = load_model('models/short_floor.urdf', fixed_base=True)
+        
+        self.robots = [robot, floor]
     
 
     
-# block = load_model(BLOCK_URDF, 
-#                    pose=([0,0,0.1], [0,0,0,1]),
-#                    fixed_base=False)
-# set_pose(block, Pose(Point(y=0.5, z=stable_z(block, floor))))
+    # block = load_model(BLOCK_URDF, 
+    #                    pose=([0,0,0.1], [0,0,0,1]),
+    #                    fixed_base=False)
+    # set_pose(block, Pose(Point(y=0.5, z=stable_z(block, floor))))
 
-part_bottom = p.loadSDF(SDF_part_bottom, physicsClientId=0)
-p.resetBasePositionAndOrientation(part_bottom[0], 
-                                [0,0,0.01], 
-                                [1, 0, 0, 0 ],
-                                )
-
-
-camTargetPos = [0, 0, 0]
-cameraUp = [0, 0, 1]
-cameraPos = [1, 0, 0]
-p.setGravity(0, 0, -10)
-yaw = 0
-
-pitch = -45.0
-roll = 0
-upAxisIndex = 2
-camDistance = 1.5
-pixelWidth = 640
-pixelHeight = 480
-nearPlane = 0.01
-farPlane = 100
-fov = 60
-viewMatrix = p.computeViewMatrixFromYawPitchRoll(camTargetPos, camDistance, yaw, pitch,
-                                                            roll, upAxisIndex)
-aspect = pixelWidth / pixelHeight
-projectionMatrix = p.computeProjectionMatrixFOV(fov, aspect, nearPlane, farPlane)
+    part_bottom = p.loadSDF(SDF_part_bottom, physicsClientId=0)
+    self.robots.append(part_bottom)
     
-img_arr = p.getCameraImage(pixelWidth,pixelHeight,viewMatrix,projectionMatrix)
-# TODO: save image
-# w = img_arr[0]  #width of the image, in pixels
-# h = img_arr[1]  #height of the image, in pixels
-# rgb = img_arr[2]  #color data RGB
-# dep = img_arr[3]  #depth data
-# print("w=",w,"h=",h)
-# np_img_arr = np.reshape(rgb, (h, w, 4))
-# np_img_arr = np_img_arr * (1. / 255.)
-# pylab.imshow(np_img_arr, interpolation='none', animated=True, label="pybullet")
-
-
-def step(at:np.ndarray, stepsize=50, realtime=True):
-  # TODO: add suck action
-  # get current joint state
-  px, vx, fx, tx = p.getJointState(0, 0)
-  py, vy, fy, ty = p.getJointState(0, 1)
-  pz, vz, fz, tz = p.getJointState(0, 2)
-  p.setJointMotorControl2(0, 0, p.POSITION_CONTROL, targetPosition=px + at[0])
-  p.setJointMotorControl2(0, 1, p.POSITION_CONTROL, targetPosition=py + at[1])
-  p.setJointMotorControl2(0, 2, p.POSITION_CONTROL, targetPosition=pz + at[2])
-  for i in range (stepsize):
-      p.stepSimulation()
-      if realtime:
-        time.sleep(1./240.)
-  return
-
-action_space = gym.spaces.Box(low=np.array([-0.05, -0.05, -0.05]), 
+    p.resetBasePositionAndOrientation(part_bottom[0], 
+                                    [0,0,0.01], 
+                                    [1, 0, 0, 0 ],
+                                    )
+    
+    p.setGravity(0, 0, -10)
+    
+    self.action_space = gym.spaces.Box(low=np.array([-0.05, -0.05, -0.05]), 
                              high=np.array([0.05, 0.05, 0.05]), dtype=np.float)
 
-for i in range(1000):
-  at = action_space.sample()
-  # uncomment to get visual info
-  img_arr = p.getCameraImage(pixelWidth,pixelHeight,viewMatrix,projectionMatrix)
-  step(at)
+    self.external_force = None
+    
+    ## NOTE: required for external force to work 
+    p.setRealTimeSimulation(0)
+    
   
+  def setup_camera(self):
+    """
+    set up camera parameters
+    """
+    camera_params = {
+      'camTargetPos': [0, 0, 0],
+      'yaw': 0,
+      'pitch': -45.0,
+      'roll': 0,
+      'upAxisIndex': 2,
+      'camDistance': 1.5,
+      'pixelWidth': 640,
+      'pixelHeight': 480,
+      'nearPlane': 0.01,
+      'farPlane': 100,
+      'fov': 60,
+    }
+
+    viewMatrix = p.computeViewMatrixFromYawPitchRoll(camera_params['camTargetPos'], 
+                    camera_params['camDistance'], 
+                    camera_params['yaw'], 
+                    camera_params['pitch'],
+                    camera_params['roll'], 
+                    camera_params['upAxisIndex'])
+    aspect = camera_params['pixelWidth'] / camera_params['pixelHeight']
+    projectionMatrix = p.computeProjectionMatrixFOV(camera_params['fov'], 
+                          aspect,
+                          camera_params['nearPlane'], 
+                          camera_params['farPlane'])
+    
+    self.camera_params = camera_params
+    self.viewMatrix = viewMatrix
+    self.aspect = aspect
+    self.projectionMatrix = projectionMatrix
+    
+    return camera_params
+  
+  def get_image(self):
+    img_arr = p.getCameraImage(self.camera_params['pixelWidth'],
+        self.camera_params['pixelHeight'],
+        self.viewMatrix,
+        self.projectionMatrix,)
+    return img_arr
+  
+  def save_image(self, saveloc=None):
+    img_arr = self.get_image()
+    w = img_arr[0]  #width of the image, in pixels
+    h = img_arr[1]  #height of the image, in pixels
+    rgb = img_arr[2]  #color data RGB
+    dep = img_arr[3]  #depth data
+    np_img_arr = np.reshape(rgb, (h, w, 4))
+    np_img_arr = np_img_arr * (1. / 255.)
+    # pylab.imshow(np_img_arr, interpolation='none', animated=True, label="pybullet")
+    if saveloc:
+      ## TODO: write RGBD file?
+      np.save(saveloc, np_img_arr)
+
+  def step(self, at:np.ndarray, stepsize=50, realtime=True):
+    # TODO: add suck action
+    # get current joint state
+    px, vx, fx, tx = p.getJointState(0, 0)
+    py, vy, fy, ty = p.getJointState(0, 1)
+    pz, vz, fz, tz = p.getJointState(0, 2)
+    p.setJointMotorControl2(0, 0, p.POSITION_CONTROL, targetPosition=px + at[0])
+    p.setJointMotorControl2(0, 1, p.POSITION_CONTROL, targetPosition=py + at[1])
+    p.setJointMotorControl2(0, 2, p.POSITION_CONTROL, targetPosition=pz + at[2])
+    for i in range (stepsize):
+        p.stepSimulation()
+        if realtime:
+          time.sleep(1./240.)
+    return
+  
+  def apply_suction(self, 
+                       body_id:int,
+                       linkIndex=-1,
+                       forceObj=[0,0,1],
+                       posObj=[0,0,0],
+                       flags=p.LINK_FRAME,
+                       ):
+    """
+    apply external force from the center of the body to the center of the end effector surface
+    """
+    p.applyExternalForce(body_id, linkIndex, forceObj, posObj, flags)
+
+
+if __name__ == '__main__':
+  env = CartesianGripper(use_debug_camera=True,
+                        use_gui=True)
+  env.setup_camera()
+  for i in range(1000):
+    # pdb.set_trace()
+    at = env.action_space.sample()
+    env.apply_suction(body_id=2, 
+                      forceObj=[0,0,-15])
+    # uncomment to get visual info
+    env.get_image()
+    env.step(at)
+    
